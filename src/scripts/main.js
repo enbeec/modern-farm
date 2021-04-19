b6974ac80cb8a6a9471ee5f6c1e65110ffa74581
Val Currie <contact@valcuri.email>
2021-04-19 14:32:39 -0500

    re-added import statements I forgot

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 import { createPlan } from './plan.js'
+import { addPlant, usePlants } from "./field.js"
+import { createCorn } from "./seeds/corn.js"
+import { createSoybean } from "./seeds/soybean.js"
+import { createAsparagus } from "./seeds/asparagus.js"
+import { createSunflower } from "./seeds/sunflower.js"
+import { createWheat } from "./seeds/wheat.js"
+import { createPotato } from "./seeds/potato.js"
 
 const myPlan = createPlan();
+
