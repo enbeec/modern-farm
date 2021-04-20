2719307d64c11c5d07123497f62ced208101b761
Val Currie <contact@valcuri.email>
2021-04-20 10:59:29 -0500

    added some clarification and formatting

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,5 +1,7 @@
 export const harvestPlants = (plants) => {
+    // empty array to store our harvest
     const theHarvest = [];
+    // loop through all given plants
     for (const plant of plants) {
         if (plant.type === "Corn") {
             // we only keep half of the corn harvest, so output is halved
@@ -7,11 +9,13 @@ export const harvestPlants = (plants) => {
                 theHarvest.push(plant);
             }
         } else {
+            // push plant.output number copies into theHarvest array
             for (let i = plant.output; i > 0; i--) {
                 // does pushing the plant into the array perform a copy each time?
                 theHarvest.push(plant);
             }
         }
    } 
+   // congrats, farmer
    return theHarvest;
 }
