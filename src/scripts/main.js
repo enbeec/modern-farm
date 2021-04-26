a6b77dd177316017f03eee3d026d4f6c0235bbe7
Val Currie <contact@valcuri.email>
2021-04-26 12:45:00 -0500

    done with project itself

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,4 +9,10 @@ plantSeeds(myPlan);
 
 // and now we create a new array to hold the proper number of harvested plants
 const myYield = harvestPlants(usePlants());
+console.log(`Here's the yield from this here harvest:`);
 console.log(myYield);
+
+// TODO: display something in the DOM
+const mainContainer = document.querySelector(".container")
+const myYieldDescribed = ''
+mainContainer.textContent = myYieldDescribed
